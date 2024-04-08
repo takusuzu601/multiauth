de73bdd9d4e99e7074b22e40c78297e5c43fd6a7
takusuzu601 <takusuzu601@gmail.com>
2024-04-08 15:09:32 +0900

    first commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\AdminLoginController;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function () {
+    return view('index');
+});
+
+
+
